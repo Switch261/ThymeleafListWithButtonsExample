1a4b4735528274f11e400ba6989ef7895ee6abbc
Switch261 <marco.nagels@outlook.de>
2021-04-15 14:35:23 +0200

    Adding: Spring Initializer Project

---
+++ src/main/java/de/demo/ListViewSingleElementsApplication.java
@@ -0,0 +1,13 @@
+package de.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListViewSingleElementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListViewSingleElementsApplication.class, args);
+	}
+
+}
